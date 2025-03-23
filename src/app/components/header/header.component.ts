daeef9a89ac0e8cb02ad6ccfdc76edb1cea430da
Víctor Cámara <73994759+VICamaraPrg@users.noreply.github.com>
2025-03-23 20:18:48 +0100

    refactor(i18n): :recycle: applied dynamic translations to all parts of the app that had static texts

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { TranslatePipe, TranslateService } from '@ngx-translate/core';
 import { ButtonModule } from 'primeng/button';
-import { DrawerModule } from 'primeng/drawer';
 import { DividerModule } from 'primeng/divider';
+import { DrawerModule } from 'primeng/drawer';
 import { TooltipModule } from 'primeng/tooltip';
 
 @Component({
@@ -13,9 +14,12 @@ import { TooltipModule } from 'primeng/tooltip';
     RouterModule,
     DividerModule,
     TooltipModule,
+    TranslatePipe,
   ],
   templateUrl: './header.component.html',
 })
 export class HeaderComponent {
+  private readonly translate = inject(TranslateService);
+
   visible = false;
 }
