d8f4a02521e444d3a63cbc1024cb0eaa3124266f
Víctor Cámara <73994759+VICamaraPrg@users.noreply.github.com>
2025-03-23 17:55:39 +0100

    chore(i18n): :wrench: configuration and initial setup of ngx-translate for i18n

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,18 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter } from '@angular/router';
-import { provideTranslateService } from '@ngx-translate/core';
+import { provideTranslateService, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import Aura from '@primeng/themes/aura';
 import { providePrimeNG } from 'primeng/config';
 
-import { provideHttpClient } from '@angular/common/http';
+import { HttpClient, provideHttpClient } from '@angular/common/http';
 import { ConfirmationService, MessageService } from 'primeng/api';
 import { routes } from './app.routes';
+
+const httpLoaderFactory: (http: HttpClient) => TranslateHttpLoader = (
+  http: HttpClient,
+) => new TranslateHttpLoader(http, './i18n/', '.json');
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -21,7 +26,11 @@ export const appConfig: ApplicationConfig = {
       },
     }),
     provideTranslateService({
-      defaultLanguage: 'es',
+      loader: {
+        provide: TranslateLoader,
+        useFactory: httpLoaderFactory,
+        deps: [HttpClient],
+      },
     }),
 
     MessageService,
