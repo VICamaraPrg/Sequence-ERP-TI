562b0403e20eb19f561c036a267c27a091e6e8e1
Víctor Cámara <73994759+VICamaraPrg@users.noreply.github.com>
2025-03-27 19:13:40 +0100

    feat: :sparkles: implemented confirm-dialog service and notification service
    The idea is to delegate its responsability to the service instead of the component, making them extremely clear and neat

---
+++ src/app/services/confirm-dialog.service.ts
@@ -0,0 +1,43 @@
+import { Injectable, inject } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { ConfirmationService } from 'primeng/api';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CustomConfirmDialogService {
+  private confirmationService = inject(ConfirmationService);
+  private translate = inject(TranslateService);
+
+  confirm(
+    target: EventTarget,
+    confirmMessage: string,
+    confirmAction: string,
+    acceptCallback: () => void,
+    cancelCallback?: () => void,
+  ): void {
+    this.confirmationService.confirm({
+      target,
+      message: confirmMessage,
+      icon: 'pi pi-exclamation-triangle',
+      rejectButtonProps: {
+        label: this.translate.instant('songModal.form.action.cancel'),
+        severity: 'secondary',
+        outlined: true,
+      },
+      acceptButtonProps: {
+        label: confirmAction,
+        severity: 'danger',
+      },
+      accept: () => {
+        console.log('accept callback');
+        acceptCallback();
+      },
+      reject: () => {
+        if (cancelCallback) {
+          cancelCallback();
+        }
+      },
+    });
+  }
+}
